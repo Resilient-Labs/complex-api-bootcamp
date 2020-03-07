83e8846a5177252da40a0e5d600e85a196f65acc
aiperieldar <aiperieldar.dev@gmail.com>
2020-03-07 15:43:12 -0500

    new commit

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,27 @@
-// let userPickZipCode = prompt("Type in the desired zipcode!")
+let ul = document.querySelector('ul')
 
-function newItem(site, weather) {
-  const ul = document.querySelector('#addInput')
-  console.log(site, weather)
-  let itemAdd = document.createElement("li")
-  const addImg = document.createElement("img")
-  addImg.src = weather.data.current_condition[0].weatherIconUrl[0].value
-  itemAdd.innerHTML = site.center + ', Facility: ' + site.facility + ',' + '<br>' + site.city + ',' + site.state + ', ' + site.zipcode + '<br>' + weather.data.current_condition[0].temp_F + '°F' + '<br>'// added the site on the nasa location and attached the weather from the array. As well as the max temp of the location
-  ul.appendChild(itemAdd)
-  itemAdd.appendChild(addImg)
-  document.querySelector("#addInput").value = ""
-}
-
-fetch(`https://data.nasa.gov/resource/gvk9-iz74.json`)
-  .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-  .then(nasaSites => {
-    //  console.log(nasaSites.forEach((center) => { //for each will call
-    //  console.log(center.zipcode)
-    for (let i = 0; i < nasaSites.length; i++) {
-    // for (let i = 0; i < nasaSites.length; i++) {
-      console.log(nasaSites[i])
-      fetch(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=426a3cf867fe4b93995205328200303&q=${nasaSites[i].zipcode}&num_of_days=7&tp=3&format=json`)
-        .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-        .then(weather => {
-          newItem(nasaSites[i], weather) //Passing in two different apis. Getting the site location and the weather of the sites
-        })
-        .catch(err => {
-          console.log(`error ${err}`)
-          //alert("sorry, there are no results for your search")
-        });
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=48f9f2d101e28f0aec99d6686573ffce&language=en-US&page=1`)
+  .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+  .then(response => {
+    for (var i = 0; i < response.results.length; i++) {
+      let movieList = document.createElement('li')
+      ul.appendChild(movieList)
+      movieList.innerHTML = response.results[i].title
+      movieList.addEventListener( "click", function () {
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=LTBHmTUIGHyN7nx50vnvss4CyHjwWDao&q=${movieList.innerHTML}&limit=25&offset=0&rating=G&lang=en`)
+          .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+          .then(res => {
+            console.log(res.data[1].images.downsized_large.url);
+            document.querySelector('img').src = res.data[1].images.downsized_large.url
+          })
+          .catch(err => {
+            console.log(`error ${err}`)
+            alert("sorry, there are no results for your search")
+          });
+      })
     }
   })
   .catch(err => {
     console.log(`error ${err}`)
-    // alert("sorry, there are no results for your search")
+    alert("sorry, there are no results for your search")
   });
-
-
-
-// fetch(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=426a3cf867fe4b93995205328200303&q=${userPickZipCode}&num_of_days=7&tp=3&format=json`)
-//     .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-//     .then(nasaSites => {
-//       console.log(nasaSites.data.weather)
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//         alert("sorry, there are no results for your search")
-//     });
