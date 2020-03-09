10a192a5e8a95bb332462eb7d772ae380949f178
Sebastian Vivas <60440088+sebastian-vivas@users.noreply.github.com>
2020-03-08 22:53:11 -0400

    Update main.js

--- complexApiOne/js/main.js
+++ complexApiOne/js/main.js
@@ -1,6 +1,5 @@
-const ulOne = document.getElementById("listOne")
-const ulTwo = document.getElementById("listTwo")
-
+const ol = document.getElementById("listOne")
+const ul = document.getElementById("listTwo")
 
 document.getElementById("findWord").addEventListener("click", () => {
   let word = document.querySelector("input").value
@@ -8,38 +7,36 @@ document.getElementById("findWord").addEventListener("click", () => {
    fetch(`https://www.dictionaryapi.com/api/v3/references/medical/json/${word}?key=e5c734ea-998a-4c8a-b484-5e66bfde9746`)
          .then(res => res.json())
          .then(dictionary => {
-           console.log(dictionary)
-      document.getElementById("paragraph").innerHTML = word + " " + dictionary[0].hwi.prs[0].mw
+
+      document.getElementById("searchWord").innerHTML = word + " [" + dictionary[0].hwi.prs[0].mw + "]"
 
       for (let i = 0; i < dictionary.length; i++) {
-      let entry = dictionary[i];
-      let li = document.createElement('li');
-      ulOne.appendChild(li);
-      li.innerHTML = li.innerHTML + entry.meta.id + " " + entry.fl + " " + entry.shortdef
+        let entry = dictionary[i];
+        let li = document.createElement('li');
+        ol.appendChild(li);
+        li.innerHTML = li.innerHTML + entry.meta.id + ": " + entry.fl + ", " + entry.shortdef
     }
 
     fetch(`https://words.bighugelabs.com/api/2/3d63cb92223d853dbba314b9a38109ed/${dictionary[0].meta.id}/json/`)
         .then(res => res.json())
         .then(response => {
-          console.log(response)
 
+       document.getElementById("synonyms").innerHTML = "Synonyms"
        document.getElementById("synNoun").innerHTML = "Nouns:" + " " + response.noun.syn
        document.getElementById("synVerb").innerHTML = "Verbs:" + " " + response.verb.syn
        document.getElementById("synAdj").innerHTML = "Adjectives:" + " " + response.adjective.syn
-
 
   })
 })
        .catch(err => {
         console.log(`error ${err}`)
-    })
-
+  })
 })
 
-  document.getElementById("findWord").addEventListener("click", () => {
+document.getElementById("findWord").addEventListener("click", () => {
   const li = document.querySelectorAll("LI");
   for(let i = 0; i < li.length; i++){
-    ulOne.removeChild(li[i]);
+    ol.removeChild(li[i]);
   }
 })
 
@@ -49,5 +46,6 @@ document.getElementById("findWord").addEventListener("click", () => {
   let newLi = document.createTextNode(addToList);
 
   li.appendChild(newLi);
-  ulTwo.appendChild(li);
+  ul.appendChild(li);
+  document.getElementById("history").innerHTML = "Search History"
 })
