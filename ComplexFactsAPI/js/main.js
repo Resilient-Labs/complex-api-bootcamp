3bb07cc4505e7fcb93c588ef5fce8ee8cdbb2984
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-09-07 17:58:12 -0400

    Updated Index file

--- ComplexFactsAPI/js/main.js
+++ ComplexFactsAPI/js/main.js
@@ -1,12 +1,17 @@
 let search = document.querySelector("input").value;
 let btn = document.querySelector("button")
 btn.addEventListener("click",()=>{
+
+
+//fetches the random fact
 fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
   .then(res => res.json())
   .then(response =>{
     // console.log(response.text)
     document.querySelector("p").textContent = response.text
     const apikey = "BD7of2s8BxdXdgo4Y0uW60V9QqwVXsBK"
+
+//fetches the gif
 fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apikey}`)
   .then(res =>res.json())
   .then(response => {
