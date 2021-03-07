82c450e23cece6680de17243f23963a28fe7ae68
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-03-06 23:26:35 -0500

    final draft prior to submit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-let url = `https://cors.bridged.cc/https://thronesapi.com/api/v2/Characters/${id}
+let url = `https://cors.bridged.cc/https://thronesapi.com/api/v2/Characters/
 `
-let url2 = `https://www.anapioficeandfire.com/api/houses`
+// let url2 = `https://www.anapioficeandfire.com/api/characters?pageSize=300`
 
 document.querySelector("button").addEventListener("click", findCharacter)
 
 function findCharacter(){
-  //get information from input -> turn name into corresponding ID use conditional?
+  // get information from input -> turn name into corresponding ID use conditional?
   const userInput = document.querySelector("input").value
-    if(userInput === data.[].firstName || userInput === data.[].lastName || data.[].fullName ){
-      let id = data.[i].id
-      
+
+
       //take the id put it in the URL and fetch that sketch
       fetch(url)
       .then(res => res.json())
       .then(data =>{
-        console.log(data);// structure: data[i].firstName | other relevant properties, lastName, fullName, imageUrl, title
+        // structure: data[i].firstName | other relevant properties, lastName, fullName, imageUrl, title
+        for (let i = 0; i < data.length; i++) {
+          console.log(data[i]);
+          if(data[i].fullName === userInput || userInput === data[i].lastName || data[i].firstName ){
+            document.querySelector(".name").innerText = data[].fullName
+          }
+        }
 
-
-      //placing data in the DOM
-      document.querySelector(".name").innerText = data[].fullName
-      document.querySelector(".title").innerText = data[].title
-      document.querySelector("img").src = data[].imageUrl
+      // //placing data in the DOM
+      // document.querySelector(".name").innerText = data[].fullName
+      // document.querySelector(".title").innerText = data[].title
+      // document.querySelector("img").src = data[].imageUrl
 
       })
       .catch(err =>{
@@ -28,7 +32,7 @@ function findCharacter(){
       })
     }
 
-}
+
 
 
 
