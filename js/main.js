e349ba359e7d7d6f6e503313244d076c7fd3280e
Natasha Torres <natashaxtorres@gmail.com>
2021-10-01 18:09:40 -0400

    complex api

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+document.querySelector('button').addEventListener('click', getLyrics)
+
+
+function getLyrics() {
+
+    let character = document.querySelector('input').value
+    const url = `https://animechan.vercel.app/api/quotes/anime?title=${character}`
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('h2').innerText = data[0].character
+            document.querySelector('p').innerText = `"${data[0].quote}"`
+            
+            const photoUrl = `http://api.giphy.com/v1/gifs/search?api_key=Y9VUwomfizbVbFIhKqMA0So3Uw5IUr30&q=${data[0].character}`
+            
+            fetch(photoUrl)
+                .then(res => res.json())
+                .then(gifData => {
+                    document.querySelector('iframe').src = gifData.data[0].embed_url
+                })
+
+                .catch(err => {
+                    console.log(`error ${err}`)
+                });
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
+
+
