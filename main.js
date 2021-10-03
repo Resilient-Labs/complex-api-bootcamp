bf07bc0035b741a0450c660a01b71a688d0fa705
Miriam Kifle <88691480+MiriamKifle@users.noreply.github.com>
2021-10-03 01:27:20 -0400

    edit

---
+++ main.js
@@ -0,0 +1,28 @@
+//function to grab the data
+document.querySelector('button').addEventListener('click', getImage)
+
+function getImage(){
+    fetch(`https://random-word-form.herokuapp.com/random/animal`)
+    .then (response => response.json()) 
+    .then (data => { 
+        console.log(data[0])
+        document.querySelector('.animalName').innerHTML = data[0]
+        //document.querySelector('#last').innerHTML = data.names[1]
+        let random = data[0]
+        console.log(random)
+
+        let animalNameUrl = `https://api.unsplash.com/search/photos?query=${random}&client_id=Reuu1lyPSuWjgqTznzVAdoHULIOEeZbvN1XLiPXiWwg`
+
+        fetch(animalNameUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results[0])
+            document.querySelector('.animalPic').src = data.results[0]['urls'].raw
+        })
+
+    
+
+});
+
+};
+
