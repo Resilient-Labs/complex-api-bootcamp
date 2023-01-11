2c5ed6e0f3db937948e8734bf1c025208c944b2b
khaled <khaledabdel.dev@gmail.com>
2023-01-11 17:41:42 -0500

    updated styling

--- Complex Salah API/js/main.js
+++ Complex Salah API/js/main.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('click', getTimes)
+document.querySelector('#button').addEventListener('click', getTimes)
 
 
 
@@ -14,12 +14,12 @@ function getTimes(){
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            document.querySelector('.location').innerText = 'Prayer Times in ' + data.settings.location.city
-            document.querySelector('.fajr').innerText = 'Fajr: ' + data.results.Fajr.substr(0,5) + ' AM'
-            document.querySelector('.duhr').innerText = 'Duhr: ' + data.results.Dhuhr.substr(0,5) + ' PM'
-            document.querySelector('.asr').innerText = 'Asr: ' + data.results.Asr.substr(0,5) + ' PM'
-            document.querySelector('.maghrib').innerText = 'Maghrib: ' + data.results.Maghrib.substr(0,5) + ' PM'
-            document.querySelector('.isha').innerText = 'Isha: ' + data.results.Isha.substr(0,5) + ' PM'
+            document.querySelector('.location').innerText = 'Prayer in ' + data.settings.location.city
+            document.querySelector('.fajr').innerText = data.results.Fajr.substr(0,5) + ' AM'
+            document.querySelector('.duhr').innerText = data.results.Dhuhr.substr(0,5) + ' PM'
+            document.querySelector('.asr').innerText = data.results.Asr.substr(0,5) + ' PM'
+            document.querySelector('.maghrib').innerText = data.results.Maghrib.substr(0,5) + ' PM'
+            document.querySelector('.isha').innerText = data.results.Isha.substr(0,5) + ' PM'
         })
     .catch(err => {
         console.log(`error ${err}`)
